223ced3517bdaf64e225cff11c9537728dfdda42
EC2 Default User <ec2-user@ip-172-31-14-129.ap-northeast-1.compute.internal>
2022-09-06 13:22:58 +0000

    [Update]index.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
-  def top
+  def new
+    @list = List.new
   end
+
+  def create
+    @lists = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+  private
+    def list_params
+      params.require(:list).permit(:title, :body)
+    end
 end
