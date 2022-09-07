06a045f210cd9a4c46bfa4f76bde9b3ad7abf0e5
EC2 Default User <ec2-user@ip-172-31-14-129.ap-northeast-1.compute.internal>
2022-09-07 12:49:33 +0000

    [残り]バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,33 @@
 class BooksController < ApplicationController
-  def new
-    @list = List.new
-  end
-
   def create
     @lists = List.new(list_params)
-    list.save
-    redirect_to '/top'
+    @lists.save
+    redirect_to "/books/#{@lists.id}"
   end
 
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/books'
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books' => 'books#index'
-  get 'books' => 'books#create'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'list'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+  patch 'books/:id' => 'books#update', as: 'update_list'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
